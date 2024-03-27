7ef59b3798e3c3a0da864cabdf6c8e95bf213acd
boriskj <boris_krj@protonmail.com>
2024-03-27 18:03:17 +0100

    light swich in text

--- src/components/LightSwitch.jsx
+++ src/components/LightSwitch.jsx
@@ -2,10 +2,22 @@ import { useEffect, useState } from "react";
 import { Switch } from "./ui/switch";
 
 export function LightSwitch() {
+  const [isDark, setIsDark] = useState(false);
+
+  useEffect(() => {
+    const root = document.documentElement; // najde <html> značko
+    root.classList.remove("light", "dark"); // odstrani prejšnji class
+    root.classList.add(isDark ? "dark" : "light"); // dodaj nov class glede na vrednost isDark
+  }, [isDark]);
+
   // Naredi state `isDark`
 
   // Uporabi useEffect za dodajanje ali odstranjevanje classa `dark` na `document.documentElement` glede na vrednost `isDark`
 
   // Prikazi Switch komponento in ji posreduj `isDark` in `setIsDark` kot props
-  return <div>To je LightSwitch.jsx</div>;
+  return (
+    <div>
+      <Switch checked={isDark} onCheckedChange={setIsDark} />
+    </div>
+  );
 }
--- src/components/TextLengthCounter.jsx
+++ src/components/TextLengthCounter.jsx
@@ -1,5 +1,36 @@
 import { useEffect, useState } from "react";
+import { Textarea } from "./ui/textarea";
+import { Slider } from "@/components/ui/slider";
 
 export function TextLengthCounter() {
-  return <div>To je TextLengthCounter.jsx</div>;
+  const [text, setText] = useState("");
+  const [maxChars, setMaxChars] = useState(100);
+  const [isOverLimit, setIsOverLimit] = useState(false);
+
+  useEffect(() => {
+    console.log(text.length);
+    if (text.length > maxChars) {
+      console.log("PREVEC ZNAKOV");
+      setIsOverLimit(true);
+    }
+  }, [text]);
+  return (
+    <div>
+      <Textarea
+        value={text}
+        onChange={(v) => setText(v.target.value)}
+      ></Textarea>
+      <Slider
+        value={[maxChars]}
+        onValueChange={(new_value) => setMaxChars(new_value[0])}
+      ></Slider>
+      {isOverLimit ? "predolgo besedilo" : "dobro besedilo"}
+
+      {/* if (isOverLimit == true) {         je enako kot odzgori
+        predolg telst
+      } else { 
+        ok t ekst 
+        }*/}
+    </div>
+  );
 }
